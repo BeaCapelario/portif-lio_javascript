dbe395a10820962c188ecf314e6d5031f49aefc7
BeaCapelario <beatrizcapelario@gmail.com>
2025-09-20 12:03:47 -0300

    Adicionando Exercicios

---
+++ exercicios.js
@@ -0,0 +1,169 @@
+// Lista 1
+// Exercício 1
+document.getElementById("ex01").addEventListener("click", function() {
+  const nome = prompt("Qual é o seu nome?");
+  alert(`Bem vindo(a), ${nome}!`);
+});
+
+// Exercicio 2
+document.getElementById("ex02").addEventListener("click", function() {
+    const materia = prompt("Qual sua matéria preferida?")
+    alert(`A materia ${materia} realmente é ótima!`)
+});
+
+// Exercicio 3
+document.getElementById("ex03").addEventListener("click", function (){
+    let data = prompt("Poderia me informar a data de hoje?")
+    alert(`A data de hoje é ${data}`)
+});
+
+// Exercicio 4
+document.getElementById("ex04").addEventListener("click", function (){
+    alert("Olá!")
+    confirm("Está preparado para o segundo semestre de desenvolvimento de sistemas?") 
+});
+//Lista 2
+// Exercicio 1
+document.getElementById("ex05").addEventListener("click", function(){
+    let num1 = Number(prompt("Insira o 1º número:"));
+    let num2 = Number(prompt("Insira o 2º número:"));
+
+    let soma = num1 + num2;
+    let sub = num1 - num2;
+    let multi = num1 * num2;
+    let divisao = num1 / num2;
+
+    alert(`O resultado da soma é: ${soma}`);
+    alert(`O resultado da subtração é: ${sub}`);
+    alert(`O resultado da multiplicação é: ${multi}`);
+    alert(`O resultado da divisão é: ${divisao}`);
+});
+
+// Exercicio 2
+document.getElementById("ex06").addEventListener("click" , function(){
+    let nota1 = Number(prompt("Inisra a 1ª nota:"))
+    let nota2 = Number(prompt("Inisra a 2ª nota:"))
+    let nota3 = Number(prompt("Inisra a 3ª nota:"))
+
+    let soma = nota1 + nota2 + nota3
+    let media = soma / 3
+
+    alert(`A média do aluno é: ${media}`)
+});
+
+// Exercicio 3
+document.getElementById("ex08").addEventListener("click" , function(){
+    largura = Number(prompt("Insira a medida da largura da parede:"))
+altura = Number(prompt("Insira a medida da altura da parede:"))
+
+let area = largura * altura;
+let litros_tinta = area / 2;
+
+alert(`A quantidade necessária para pintar a parede é: ${litros_tinta} litros`)
+
+});
+
+// Exercicio 4
+document.getElementById("ex07").addEventListener("click" , function(){
+    num1 = Number(prompt("Insira o 1º número:"))
+    num2 = Number(prompt("Insira o 2º número:"))
+
+    let diferenca = num2 - num1
+
+    alert(`A diferenças entre os números é ${diferenca}`)
+});
+
+// Exercicio 5
+document.getElementById("ex09").addEventListener("click" , function(){
+   
+});
+
+// Exercicio 6
+document.getElementById("ex10").addEventListener("click" , function(){
+    celsius = Number(prompt("Insira a temperatura em Celsius"))
+
+    conversao = ((celsius * 9 / 5 ) + 32)
+
+    alert(`A temperatura em Fahrenheit é ${conversao} °F`)
+});
+
+// Exercicio 7
+document.getElementById("ex11").addEventListener("click" , function(){
+  base = Number(prompt("Insira o valor da base:"))
+    altura = Number(prompt("Insira o valor da altura:"))
+
+    calculoArea = base * altura
+
+alert(`A area do retangulo é: ${calculoArea}`)
+});
+
+//Lista 3
+//Exercicio 1
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+//Exercicio 2
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+//Exercicio 3
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 4
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 5
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Lista 4
+// Exercicio 1
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 2
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 3
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 4
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Lista 5
+// Exercicio 1
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 2
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 3
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Exercicio 4
+document.getElementById("ex06").addEventListener("click" , function(){
+  
+});
+
+// Lista 6
+
